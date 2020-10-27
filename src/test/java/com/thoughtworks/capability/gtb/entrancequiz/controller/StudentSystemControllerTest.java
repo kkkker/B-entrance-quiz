460685041b555a209d9798877bfb04aac92eea0c
kker <907765631@qq.com>
2020-10-27 19:53:16 +0800

    [Mamingjun] feat: implement CorsConfig

--- src/test/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentSystemControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentSystemControllerTest.java
@@ -7,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
